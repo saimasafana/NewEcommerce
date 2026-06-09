397146c13c817bf2bb99ab9d53533b2b7e5e6223
saimasafanakk@gmail.com <saimaoneteam@gmail.com>
2026-06-09 07:21:10 +0530

    added diployment settings and build.sh for render3

--- backend/mainproject/mainproject/deployment_settings.py
+++ backend/mainproject/mainproject/deployment_settings.py
@@ -1,6 +1,6 @@
 import os
 import dj_database_url
-from settings import *
+from .settings import *
 ALLOWED_HOSTS=[os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 CSRF_TRUSTED_ORIGINS='https://'+os.environ.get['RENDER_EXTERNAL_HOSTNAME']
 DEBUG=False
