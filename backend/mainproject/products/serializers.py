fa32119c35f87c744d4c817ec63e6daa861b3654
saimasafanakk@gmail.com <saimaoneteam@gmail.com>
2026-06-09 18:53:15 +0530

    change image url5

--- backend/mainproject/products/serializers.py
+++ backend/mainproject/products/serializers.py
@@ -6,8 +6,12 @@ class CategorySerializer(serializers.ModelSerializer):
         fields='__all__'
         
 class ProductSerializer(serializers.ModelSerializer):
-    
+    image = serializers.SerializerMethodField()
+
     class Meta:
-        model=Product;
-        fields='__all__'
-    
+        model = Product
+        fields = '__all__'
+
+    def get_image(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.image.url)
